b72cf671154d0acba6e37891910ad015094f65d9
Craig Lomax <craig@clomax.me.uk>
2014-06-01 11:27:33 +0100

    Fix this shit.

--- app/overrides/alter_footer.rb
+++ app/overrides/alter_footer.rb
@@ -1,5 +1,3 @@
 Deface::Override.new (virtual_path: 'spree/layouts/spree_application',
-                      replace:      'erb[loud]:contains("spree/shared/footer")',
-                      partial:      'views/partials/_footer',
                       name:         'alter_footer'
                      )
