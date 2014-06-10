a0451dfe499d60d8f27d92074895f2a20452915c
Craig Lomax <craig@clomax.me.uk>
2014-06-10 18:29:18 +0100

    Add css class to taxon links.

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -7,7 +7,7 @@ module Spree
         root_taxon.children.map do |taxon|
           css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'current' : nil
           content_tag :li, class: css_class do
-            link_to(taxon.name, seo_url(taxon), :class => "#{current_taxon}") +
+            link_to(taxon.name, seo_url(taxon), :class => "taxonomy-child") +
             taxons_tree(taxon, current_taxon, max_level - 1)
           end
         end.join("\n").html_safe
