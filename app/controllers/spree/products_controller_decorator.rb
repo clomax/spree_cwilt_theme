cf29096e0598d3a0ff306d7693d476527fda55f1
Craig Lomax <craig@clomax.me.uk>
2014-06-11 17:45:11 +0100

    Add sidebar to product page.

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -4,7 +4,7 @@ module Spree
     def show
       @variants = @product.variants_including.active(current_currency).includes([:option_values, :images])
       @product_properties = @product.product_properties.includes(:property)
-      @taxon = Spree::Taxon.find(params[:taxon_id] if params[:taxon_id])
+      @taxon = Spree::Taxon.find(params[:taxon_id]) if params[:taxon_id]
       @taxonomies = Spree::Taxonomy.includes(root: :children)
     end
 
