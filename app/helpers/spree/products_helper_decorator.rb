b55e08c61be833b76df42faadef69c77d6feb0d3
Craig Lomax <craig@clomax.me.uk>
2014-06-12 09:45:56 +0100

    Add brand to product title.

--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -3,7 +3,7 @@ module Spree
 
     def property_value_for(product, property)
       return nil unless property = product.properties.find_by_name(property)
-      return nil unless product_properties = product.product_properties.find_by_property_id(proterty.id)
+      return nil unless product_properties = product.product_properties.find_by_property_id(property.id)
       product_properties.value
     end
 
