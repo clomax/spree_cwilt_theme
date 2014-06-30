99a02bf80cc18f6245db7df4d5f356ce880616c1
Craig Lomax <craig@clomax.me.uk>
2014-06-30 17:07:58 +0100

    Add images to showcase.

--- app/assets/javascripts/spree/frontend/spree_cwilt_theme.js
+++ app/assets/javascripts/spree/frontend/spree_cwilt_theme.js
@@ -2,5 +2,5 @@
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
 
 $(document).ready(function() {
-  $("showcase_inner_container").css("transform", "translateX("+%(this).index() * -663+"px)");
+  $("showcase_inner_container").css("transform", "translateX("+$(this).index() * -663+"px)");
 });
