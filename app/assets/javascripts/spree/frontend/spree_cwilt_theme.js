14ee2a57892b0525bc83f54a7381c0ec03af953d
Craig Lomax <craig@clomax.me.uk>
2014-06-30 17:15:03 +0100

    Add images to showcase.

--- app/assets/javascripts/spree/frontend/spree_cwilt_theme.js
+++ app/assets/javascripts/spree/frontend/spree_cwilt_theme.js
@@ -1,6 +1,9 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
 
-$(document).ready(function() {
-  $("showcase_inner_container").css("transform", "translateX("+$(this).index() * -663+"px)");
+$(window).load(function() {
+  function loop() {
+    $("showcase_inner_container").css("transform", "translateX("+$(this).index() * -663+"px)");
+  };
+  window.setTimeout( loop, 1000 );
 });
