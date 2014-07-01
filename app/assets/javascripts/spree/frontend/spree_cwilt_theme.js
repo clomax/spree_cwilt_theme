ae700ad4b02ef7848ebb70f558456562ee0e7a52
Craig Lomax <craig@clomax.me.uk>
2014-07-01 13:19:53 +0100

    Add images to showcase.

--- app/assets/javascripts/spree/frontend/spree_cwilt_theme.js
+++ app/assets/javascripts/spree/frontend/spree_cwilt_theme.js
@@ -1,6 +1,8 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
 
+//= require lightbox.min.js
+
 var index = 0;
 var next  = 1;
 
