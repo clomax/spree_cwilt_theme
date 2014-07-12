36188b422d56f686008efe0d23d2d53d440878d4
Craig Lomax <craig@clomax.me.uk>
2014-07-12 20:10:48 +0100

    Fix showcase.

--- app/assets/javascripts/spree/frontend/spree_cwilt_theme.js
+++ app/assets/javascripts/spree/frontend/spree_cwilt_theme.js
@@ -1,32 +1,32 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
 
+//= require lightbox.min.js
+
 var index = 1;
-var delay = 5000;
-var timer;
 
 $(document).ready(function() {
-  repeat();
+  startSlider();
 });
 
-function slider() {
-  count = $("#showcase_inner_container > img").size();
-  document.getElementById("showcase_inner_container").className = "showcase_img";
-  $("#showcase_inner_container").css("transform", "translateX(" + (index * -663) + "px)");
 
-  if (index == count) {
-    index = 0;
-    delay = 1;
-    document.getElementById("showcase_inner_container").className = "showcase_img_reset";
+function startSlider() {
+  images = $("#showcase_inner_container > img");
+  count = images.size();
+
+  loop = setInterval( function() {
+    document.getElementById("showcase_inner_container").className = "showcase_img";
     $("#showcase_inner_container").css("transform", "translateX(" + (index * -663) + "px)");
-  }
+    //$("#showcase_inner_container > img#" + index).css("transform", "translateX(" + (index * -663) + "px)");
 
-  delay = 5000;
-  console.log(index + " : " + (index * -663) + "px  :  " + delay);
-  index += 1;
-}
+    if (index == count) {
+      index = 0;
+      document.getElementById("showcase_inner_container").className = "showcase_img_reset";
+      $("#showcase_inner_container").css("transform", "translateX(" + (index * -663) + "px)");
+    }
 
-function repeat() {
-  setTimeout(repeat, delay);
-  slider();
+    console.log(index + " : " + (index * -663) + "px");
+    index += 1;
+
+  }, 5000);
 }
