f85e0337fc13f2adff2838fb3d7deb9b25c6ce10
Craig Lomax <craig@clomax.me.uk>
2014-07-12 11:19:14 +0100

    Fix showcase.

--- app/assets/javascripts/spree/frontend/spree_cwilt_theme.js
+++ app/assets/javascripts/spree/frontend/spree_cwilt_theme.js
@@ -4,7 +4,6 @@
 //= require lightbox.min.js
 
 var index = 0;
-var next  = 1;
 
 $(document).ready(function() {
   startSlider();
@@ -15,15 +14,9 @@ function startSlider() {
   count = $("#showcase_inner_container > img").size();
 
   loop = setInterval( function() {
-    if(next == count+1) {
-      next = 1;
-      index = 0;
-    }
-
     $("#showcase_inner_container > img").css("transform", "translateX(" + (index * -663) + "px)");
     $("#showcase_inner_container > img#" + index).css("transform", "translateX(" + (index * -663) + "px)");
-    index = next;
-    next = next + 1;
+    index += 1;
 
   }, 5000);
 }
