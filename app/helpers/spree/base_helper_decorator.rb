6389a8d1be55133891104c368747c9d4111db068
Craig Lomax <craig@clomax.me.uk>
2014-08-04 16:57:31 +0100

    Lightbox.

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,4 +1,5 @@
-Spree::BaseHelper.module_eval do
+module Spree
+  module BaseHelper
 
     def taxons_tree(root_taxon, current_taxon, max_level = 1)
       return '' if max_level < 1 || root_taxon.children.empty?
@@ -13,28 +14,5 @@ Spree::BaseHelper.module_eval do
       end
     end
 
-    private
-
-    def create_product_image_url(image, product, options, style)
-      options.reverse_image! alt: image.alt.blank ? product.name : image.alt
-      link_to image.attachment.url(style), options
-    end
-
-    def define_image_url_method(style)
-      self.class.send :define_method, "#{style}_image_url" do |product, *options|
-        options = options.first || {}
-        if product.images.empty?
-          if !product.is_a?(Spree::Variant) && !product.variant_images.empty?
-            create_product_image_url(product.variant_images.first, product, options, style)
-          else
-            if product.is_a?(Variant) && !product.product.variant_images.empty?
-              create_product_image_url(product.product.variant_images.first, product, options, style)
-            end
-          end
-        else
-          create_product_image_url(product.images.first, product, options, style)
-        end
-      end
-    end
-
+  end
 end
